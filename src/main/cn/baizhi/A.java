2071276b0b02d399c21e9044d35debd71b15abcc
lsj <1851752509@qq.com>
2021-08-24 21:43:15 +0800

    小明 v1.0

---
+++ src/main/cn/baizhi/A.java
@@ -0,0 +1,5 @@
+package cn.baizhi;
+
+public class A {
+    int a = 0;
+}
---
+++ src/test/java/cn/baizhi/github_test1/GithubTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi.github_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
